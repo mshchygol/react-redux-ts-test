4754a0c4da1795d558ba0f3772938a6b8b58bb00
mykhaylo <mshchygol@gmail.com>
2025-09-01 17:57:53 +0300

    added router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from "react-router"
 import './index.css'
 import App from './App.tsx'
+import Registration from './pages/Registration.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route index element={<App />} />
+                <Route path="register" element={<Registration />} />
+            </Routes>
+        </BrowserRouter>
+    </StrictMode>,
 )
---
+++ src/pages/Registration.tsx
@@ -0,0 +1,9 @@
+function Registration() {
+    return (
+        <>
+            <h1>Welcome!</h1>
+        </>
+    )
+}
+
+export default Registration;
