757376062873e606857173a3c21fcc243ff05532
mykhaylo <mshchygol@gmail.com>
2025-09-01 22:34:09 +0300

    registration updated

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from "react-router"
 import './index.css'
 import App from './App.tsx'
-import Registration from './pages/Registration.tsx'
 import { Provider } from 'react-redux'
 import { store } from './store.ts'
-import RouteGuard from './RouteGuard.tsx'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <Provider store={store}>
-            <BrowserRouter>
-                <Routes>
-                    <Route element={<RouteGuard isAuthenticated={false} redirectPath='register' />} >
-                        <Route index element={<App />} />
-                    </Route>
-                    <Route path="register" element={<Registration />} />
-                </Routes>
-            </BrowserRouter>
+            <App />
         </Provider>
     </StrictMode>,
 )
--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -1,23 +1,75 @@
-import { testSlice } from "../coursesSlice";
-import { useAppDispatch, useAppSelector } from "../hooks";
+import { useState } from "react";
+import { useNavigate } from "react-router";
+
+interface ValidationErorrs {
+    email: string,
+    password: string
+}
 
 function Registration() {
-    const purchasedCourses = useAppSelector((state) => state.courses.purchasedCourses)
-    const dispatch = useAppDispatch()
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState<ValidationErorrs>({
+        email: '',
+        password: ''
+    });
+    const navigate = useNavigate()
 
-    const clickHandler = () => {
-        dispatch(testSlice())
-    }
+    const validateForm = () => {
+        const newErrors: ValidationErorrs = {
+            email: "",
+            password: ""
+        };
+
+        if (!email) {
+            newErrors.email = "Email is required"
+        } else if (!/\S+@\S+\.\S+/.test(email)) {
+            newErrors.email = "Email is invalid"
+        }
+
+        if (!password) {
+            newErrors.password = "Password is required"
+        } else if (password.length < 6) {
+            newErrors.password = "Password must be at least 6 characters"
+        } else if (!/[a-z]/.test(password)) {
+            newErrors.password = "Password must contain at least one lowercase letter"
+        } else if (!/[A-Z]/.test(password)) {
+            newErrors.password = "Password must contain at least one uppercase letter"
+        } else if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
+            newErrors.password = "Password must contain at least one special character"
+        }
+
+        setErrors(newErrors);
+
+        return !newErrors.email && !newErrors.password
+    };
+
+
+    const handleSubmit = (e: React.SyntheticEvent) => {
+        e.preventDefault()
+        if (validateForm()) {
+            localStorage.setItem('user', JSON.stringify({ email, password }))
+            navigate('/')
+        }
+    };
 
     return (
         <>
             <h1>Welcome!</h1>
-            <ul>
-                {purchasedCourses.map((course) => (
-                    <li key={course}>{course}</li>
-                ))}
-            </ul>
-            <button type="button" onClick={clickHandler}>Test slice</button>
+            <h2>Please register:</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Email:</label>
+                    <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    {errors.email && <span style={{ color: 'red' }}>{errors.email}</span>}
+                </div>
+                <div>
+                    <label>Password:</label>
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    {errors.password && <span style={{ color: 'red' }}>{errors.password}</span>}
+                </div>
+                <button type="submit">Register</button>
+            </form>
         </>
     )
 }
